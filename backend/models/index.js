7c52664e20d0e1506d57f3bf2eb9af907964b665
spragala <spragala@yahoo.com>
2017-11-05 09:02:23 -0800

    Cleaned up front-end files && added mongoose connect to backend

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,4 +1,6 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/backend");
+mongoose.connect("mongodb://localhost/backend", {
+  useMongoClient: true,
+});
 
 module.exports.TextPost = require("./textPost");
