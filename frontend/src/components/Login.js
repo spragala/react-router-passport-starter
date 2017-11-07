93b8c120619890a706a648f4e994143caffbd636
spragala <spragala@yahoo.com>
2017-11-07 15:50:05 -0800

    User form added submits object with user creds

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,11 +1,50 @@
 import React, { Component } from 'react';
 
 export default class Login extends Component{
+  constructor(){
+    super();
+    this.state = {
+      username: '',
+      password: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+
+
+  handleChange(field) {
+    return (e) => {
+      this.setState({
+        [field]: e.target.value
+      });
+    }
+  }
+
+  onFormSubmit(e) {
+    e.preventDefault()
+    let user = this.state
+    console.log('Form Submitted with this user: ', user)
+    this.setState({
+      username: '',
+      password: ''
+    })
+  };
+
   render(){
     return (
       <div>
         <h1>Login Page</h1>
-        <p> Hello World</p>
+        <form onSubmit={ e => this.onFormSubmit(e) }>
+          <label>Username</label>
+          <input
+            type='text'
+            onChange={ this.handleChange('username') } />
+          <label>Password</label>
+          <input
+            type='text'
+            onChange={ this.handleChange('password') } />
+          <button type='submit'>Log In!</button>
+        </form>
       </div>
     )
   }
