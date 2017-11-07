99153a172c1618779cf71e32fe276758950943eb
spragala <spragala@yahoo.com>
2017-11-07 14:56:09 -0800

    Installed react-router-dom && added Home and Login components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom'
+import Home from './components/Home'
+import Login from './components/Login'
 import './App.css';
 
 class App extends Component {
@@ -8,6 +11,10 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">React App</h1>
         </header>
+        <Switch>
+          <Route exact path='/' component={ Home } />
+          <Route path='/login' component={ Login } />
+        </Switch>
       </div>
     );
   }
---
+++ frontend/src/components/Login.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Login extends Component{
+  render(){
+    return (
+      <div>
+        <h1>Login Page</h1>
+        <p> Hello World</p>
+      </div>
+    )
+  }
+};
