df58ff1f4b05f5eb443f726068670861aeff0c44
spragala <spragala@yahoo.com>
2017-11-24 10:40:48 -0800

    Mobile responsiveness to navbar component

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,37 +1,50 @@
-import React from 'react'
 import Auth from '../models/Auth'
+import React from 'react'
 import { Link, withRouter } from 'react-router-dom'
 
 const Navbar = withRouter(({ history }) => (
   <nav className="navbar" aria-label="main navigation">
     <div className="navbar-brand">
       <a className="navbar-item app-title" href="/">React App</a>
+      <div
+        className="navbar-burger burger"
+        data-target="nav-content"
+        onClick={() => {
+          let burger = document.querySelector(".navbar-burger")
+          let menu = document.querySelector(".navbar-menu")
+          burger.classList.toggle("is-active")
+          menu.classList.toggle("is-active") }}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </div>
 
-    <div className="navbar-end">
-      {Auth.isUserAuthenticated() ? (
-        <a
-          className="navbar-item"
-          onClick={() => {
-            Auth.logout(() => history.push('/login'))
-          }}>Sign-out
-        </a>
-    ) : (
+    <div id="nav-content" className="navbar-menu">
       <div className="navbar-end">
-        <div className="navbar-item">
-          <div className="field is-grouped">
-            <p className="control">
-              <Link to="/login" className="button is-inverted is-primary">Log in</Link>
-            </p>
-            <p className="control">
-              <Link to="/signup" className="button is-primary">Sign up</Link>
-            </p>
+        {Auth.isUserAuthenticated() ? (
+          <a
+            className="navbar-item"
+            onClick={() => {
+              Auth.logout(() => history.push('/'))
+            }}>Sign-out
+          </a>
+      ) : (
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="field is-grouped">
+              <p className="control">
+                <Link to="/login" className="button is-inverted is-primary">Log in</Link>
+              </p>
+              <p className="control">
+                <Link to="/signup" className="button is-primary">Sign up</Link>
+              </p>
+            </div>
           </div>
         </div>
+      )}
       </div>
-    )}
     </div>
-
   </nav>
 ))
 
